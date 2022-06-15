282e7897a982d196b5b8e55432f84946de5e1532
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-06-15 22:03:05 +0300

    Component : build modal component

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,23 @@
+<div {{ $attributes }} class="modal fade" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+
+                {{-- Model Title --}}
+                <h5 class="modal-title" id="modalCenterTitle">{{ $title }}</h5>
+
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="{{ route($route) }}" method="post">
+                @csrf
+
+                {{-- Model Body --}}
+                <div class="modal-body"> {{ $body }} </div>
+
+                {{-- Model Footer --}}
+                <div class="modal-footer"> {{ $footer }} </div>
+
+            </form>
+        </div>
+    </div>
+</div>
