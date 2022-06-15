988c7fd1eb3fda08649cf93b5780d1bdffaad449
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-06-15 22:02:14 +0300

    Component : build dropdown component

---
+++ resources/views/components/dropdown-table.blade.php
@@ -0,0 +1,9 @@
+<div class="dropdown">
+    <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+        <i class="bx bx-dots-vertical-rounded"></i>
+    </button>
+    <div class="dropdown-menu">
+        {{ $slot }}
+    </div>
+
+</div>
