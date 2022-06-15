2c09e2591c92a3fe2f7ebe56d5c3cf06da577096
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-06-15 23:19:00 +0300

    Update navbar.blade.php

--- resources/views/dashboard/layout/navbar.blade.php
+++ resources/views/dashboard/layout/navbar.blade.php
@@ -8,60 +8,148 @@
 
     <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
 
+        <!-- Search -->
+        <div class="navbar-nav align-items-center">
+            <div class="nav-item navbar-search-wrapper mb-0">
+                <a class="nav-item nav-link search-toggler px-0" href="javascript:void(0);">
+                    <i class="bx bx-search bx-sm"></i>
+                    <span class="d-none d-md-inline-block text-muted">Search (Ctrl+/)</span>
+                </a>
+            </div>
+        </div>
+        <!-- /Search -->
+
+
         <ul class="navbar-nav flex-row align-items-center ms-auto">
 
+            <!-- Language -->
+            <li class="nav-item dropdown-language dropdown me-2 me-xl-0">
+                <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                    <i class='flag-icon flag-icon-us flag-icon-squared rounded-circle fs-3 me-1'></i>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    <li>
+                        <a class="dropdown-item" href="javascript:void(0);" data-language="en">
+                            <i class="flag-icon flag-icon-us flag-icon-squared rounded-circle fs-4 me-1"></i>
+                            <span class="align-middle">English</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="javascript:void(0);" data-language="fr">
+                            <i class="flag-icon flag-icon-fr flag-icon-squared rounded-circle fs-4 me-1"></i>
+                            <span class="align-middle">France</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="javascript:void(0);" data-language="de">
+                            <i class="flag-icon flag-icon-de flag-icon-squared rounded-circle fs-4 me-1"></i>
+                            <span class="align-middle">German</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="javascript:void(0);" data-language="pt">
+                            <i class="flag-icon flag-icon-pt flag-icon-squared rounded-circle fs-4 me-1"></i>
+                            <span class="align-middle">Portuguese</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <!--/ Language -->
+
+
+            <!-- Notification -->
+            <li class="nav-item dropdown-notifications navbar-dropdown dropdown me-3 me-xl-1">
+                <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+                    <i class="bx bx-bell bx-sm"></i>
+                    <span class="badge bg-danger rounded-pill badge-notifications">5</span>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end py-0">
+                    <li class="dropdown-menu-header border-bottom">
+                        <div class="dropdown-header d-flex align-items-center py-3">
+                            <h5 class="text-body mb-0 me-auto">Notification</h5>
+                            <a href="javascript:void(0)" class="dropdown-notifications-all text-body" data-bs-toggle="tooltip" data-bs-placement="top" title="Mark all as read"><i class="bx fs-4 bx-envelope-open"></i></a>
+                        </div>
+                    </li>
+                    <li class="dropdown-notifications-list scrollable-container">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item list-group-item-action dropdown-notifications-item">
+                                <div class="d-flex">
+                                    <div class="flex-shrink-0 me-3">
+                                        <div class="avatar">
+                                            <img src="../../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle">
+                                        </div>
+                                    </div>
+                                    <div class="flex-grow-1">
+                                        <h6 class="mb-1">Congratulation Lettie 🎉</h6>
+                                        <p class="mb-0">Won the monthly best seller gold badge</p>
+                                        <small class="text-muted">1h ago</small>
+                                    </div>
+                                    <div class="flex-shrink-0 dropdown-notifications-actions">
+                                        <a href="javascript:void(0)" class="dropdown-notifications-read"><span class="badge badge-dot"></span></a>
+                                        <a href="javascript:void(0)" class="dropdown-notifications-archive"><span class="bx bx-x"></span></a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown-menu-footer border-top">
+                        <a href="javascript:void(0);" class="dropdown-item d-flex justify-content-center p-3">
+                            View all notifications
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <!--/ Notification -->
+
             <!-- User -->
-{{--            <li class="nav-item navbar-dropdown dropdown-user dropdown">--}}
-{{--                <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">--}}
-{{--                    <div class="avatar avatar-online">--}}
-{{--                        <img src="@if (Auth::user()->avatar) {{ asset(UserEnum::USER_AVATAR_PATH.Auth::user()->avatar) }} @else {{ asset(UserEnum::USER_AVATAR_DEFAULT) }} @endif" alt class="w-px-40 h-auto rounded-circle">--}}
-{{--                    </div>--}}
-{{--                </a>--}}
-{{--                <ul class="dropdown-menu dropdown-menu-end">--}}
-{{--                    <li>--}}
-{{--                        <a class="dropdown-item" href="{{ route('dashboard.profile', Auth::id()) }}">--}}
-{{--                            <div class="d-flex">--}}
-{{--                                <div class="flex-shrink-0 me-3">--}}
-{{--                                    <div class="avatar avatar-online">--}}
-{{--                                        <img src="@if (Auth::user()->avatar) {{ asset(UserEnum::USER_AVATAR_PATH.Auth::user()->avatar) }} @else {{ asset(UserEnum::USER_AVATAR_DEFAULT) }} @endif" alt class="w-px-40 h-auto rounded-circle">--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                                <div class="flex-grow-1">--}}
-{{--                                    <span class="fw-semibold d-block">{{ Auth::user()->name }}</span>--}}
-{{--                                    <small class="text-muted">{{ Auth::user()->getRoleNames()[0] }}</small>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </a>--}}
-{{--                    </li>--}}
-{{--                    <li>--}}
-{{--                        <div class="dropdown-divider"></div>--}}
-{{--                    </li>--}}
-{{--                    <li>--}}
-{{--                        <a class="dropdown-item" href="{{ route('dashboard.profile',Auth::id()) }}">--}}
-{{--                            <i class="bx bx-user me-2"></i>--}}
-{{--                            <span class="align-middle">الملف الشخصي</span>--}}
-{{--                        </a>--}}
-{{--                    </li>--}}
-{{--                    <li>--}}
-{{--                        <a class="dropdown-item" href="{{ route('dashboard.setting') }}">--}}
-{{--                            <i class="bx bx-cog me-2"></i>--}}
-{{--                            <span class="align-middle">إعدادات الحساب</span>--}}
-{{--                        </a>--}}
-{{--                    </li>--}}
-{{--                    <li>--}}
-{{--                        <div class="dropdown-divider"></div>--}}
-{{--                    </li>--}}
-{{--                    <li>--}}
-{{--                        <a class="dropdown-item" href="" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" target="_blank">--}}
-{{--                            <i class="bx bx-power-off me-2"></i>--}}
-{{--                            <span class="align-middle">تسجيل الخروج</span>--}}
-{{--                        </a>--}}
-{{--                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">--}}
-{{--                            @csrf--}}
-{{--                        </form>--}}
-{{--                    </li>--}}
-{{--                </ul>--}}
-{{--            </li>--}}
+            <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                    <div class="avatar avatar-online">
+                        <img src="../../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle">
+                    </div>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    <li>
+                        <a class="dropdown-item" href="pages-account-settings-account.html">
+                            <div class="d-flex">
+                                <div class="flex-shrink-0 me-3">
+                                    <div class="avatar avatar-online">
+                                        <img src="../../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle">
+                                    </div>
+                                </div>
+                                <div class="flex-grow-1">
+                                    <span class="fw-semibold d-block">John Doe</span>
+                                    <small class="text-muted">Admin</small>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <div class="dropdown-divider"></div>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="pages-profile-user.html">
+                            <i class="bx bx-user me-2"></i>
+                            <span class="align-middle">My Profile</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="pages-account-settings-account.html">
+                            <i class="bx bx-cog me-2"></i>
+                            <span class="align-middle">Settings</span>
+                        </a>
+                    </li>
+                    <li>
+                        <div class="dropdown-divider"></div>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="auth-login-cover.html" target="_blank">
+                            <i class="bx bx-power-off me-2"></i>
+                            <span class="align-middle">Log Out</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
             <!--/ User -->
 
 
