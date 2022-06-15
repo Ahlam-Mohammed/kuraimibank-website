c13a2e7c5b064ce11736a18efef7ea04aa37e73a
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-06-15 23:19:07 +0300

    Update sidebar.blade.php

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -22,7 +22,7 @@
             </a>
             <ul class="menu-sub">
                 <li class="menu-item">
-                    <a href="" class="menu-link">
+                    <a href="{{ route('dashboard.manage-regions') }}" class="menu-link">
                         <div>@lang('page.manage_regions')</div>
                     </a>
                 </li>
