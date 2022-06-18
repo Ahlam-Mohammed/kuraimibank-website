2c28704d4d202362d3f343c3dbc2198506098bfc
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-06-18 21:06:22 +0300

    Update sidebar.blade.php

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -1,9 +1,13 @@
-<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme" style="background-color: #342a4a !important;">
 
     <div class="app-brand demo ">
-        <a href="" class="app-brand-link">
-            <span class="app-brand-text demo menu-text fw-bolder ms-2">KuraimiBank</span>
+        <a href="index-2.html" class="app-brand-link">
+            <span class="app-brand-logo demo">
+                <img src="{{ asset('images/logo.svg') }}">
+            </span>
+            <span class="app-brand-text demo menu-text fw-bolder ms-2">الكريمي</span>
         </a>
+
         <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
             <i class="bx bx-chevron-left bx-sm align-middle"></i>
         </a>
@@ -15,19 +19,21 @@
     <ul class="menu-inner py-1">
 
         {{-- service_points --}}
-        <li class="menu-item">
+        <li class="menu-item
+        @if(Route::currentRouteName() === 'dashboard.service.point' ||
+            Route::currentRouteName() === 'dashboard.region') active @endif">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
                 <i class="menu-icon tf-icons bx bx-home-circle"></i>
                 <div>@lang('page.service_points')</div>
             </a>
             <ul class="menu-sub">
                 <li class="menu-item">
-                    <a href="{{ route('dashboard.manage-regions') }}" class="menu-link">
+                    <a href="{{ route('dashboard.region') }}" class="menu-link">
                         <div>@lang('page.manage_regions')</div>
                     </a>
                 </li>
                 <li class="menu-item">
-                    <a href="" class="menu-link">
+                    <a href="{{ route('dashboard.service.point') }}" class="menu-link">
                         <div>@lang('page.service_points')</div>
                     </a>
                 </li>
