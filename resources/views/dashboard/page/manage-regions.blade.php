42c920cd1e54fe2a288bd38ecb25ef86b8e47cbb
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-06-18 21:06:14 +0300

    Update manage-regions.blade.php

--- resources/views/dashboard/page/manage-regions.blade.php
+++ resources/views/dashboard/page/manage-regions.blade.php
@@ -12,7 +12,7 @@
 @section('content')
 
     <h4 class="fw-bold py-3 mb-4">
-        <span class="text-muted fw-light">
+        <span class="fw-light">
             @lang('page.service_points') / @lang('page.manage_regions')
         </span>
     </h4>
