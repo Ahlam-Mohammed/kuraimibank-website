9aaca7cab0a5b1b89eb74ca06a0d1e4afa118f67
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-06-19 22:26:03 +0300

    .

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,3 +1,6 @@
 <div class="alert alert-dismissible" role="alert" style="display:none">
+    <div id="span">
+
+    </div>
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
